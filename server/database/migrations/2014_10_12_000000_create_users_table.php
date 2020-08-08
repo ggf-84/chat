9fc5e89b58c2ac37e5f80edbd59b33df4fcf7995
ghostflp <gutug84@gmail.com>
2020-08-08 11:26:32 +0300

    --- Auto-Git Commit ---

--- server/database/migrations/2014_10_12_000000_create_users_table.php
+++ server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('avatar');
+            $table->string('avatar')->nullable();
             $table->tinyInteger('active')->default('1');
             $table->timestamp('left_at')->nullable();
             $table->rememberToken();
